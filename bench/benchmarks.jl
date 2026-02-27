37b32216c22fc163b06d84630297554627a04c08
Alexander Kmoch <alexander.kmoch@ut.ee>
2026-02-27 14:24:34 +0200

    bench

--- bench/benchmarks.jl
+++ bench/benchmarks.jl
@@ -1,6 +1,5 @@
 using BenchmarkTools
-
-include("z7_julia.jl")
+using Z7
 
 # Define benchmark suite
 const SUITE = BenchmarkGroup()
@@ -17,7 +16,7 @@ const RESOLUTION_DIGITS = Int8[0, 1, 0, 2, 4, 4, 3, 5, 2, 3, 0, 3, 0, 4]
 const RESOLUTION_DIGITS_UINT8 = UInt8[0, 1, 0, 2, 4, 4, 3, 5, 2, 3, 0, 3, 0, 4]
 const Z7_INT_ID = encode_z7int(BASE_CELL, RESOLUTION_DIGITS_UINT8)
 
-# --- utility functions raw ---
+# --- utility functions ---
 
 SUITE["utility functions"]["decode_z7hex_index"] = @benchmarkable decode_z7hex_index($Z7_HEX_ID)
 SUITE["utility functions"]["encode_z7hex_index"] = @benchmarkable encode_z7hex_index($BASE_CELL, $RESOLUTION_DIGITS)
@@ -28,7 +27,7 @@ SUITE["utility functions"]["decode_z7int"] = @benchmarkable decode_z7int($Z7_INT
 SUITE["utility functions"]["encode_z7int"] = @benchmarkable encode_z7int($BASE_CELL, $RESOLUTION_DIGITS_UINT8)
 SUITE["utility functions"]["get_z7hex_resolution"] = @benchmarkable get_z7hex_resolution($Z7_HEX_ID)
 
-## --- Z7IndexUInt64 functions ---
+# --- Z7IndexUInt64 functions ---
 
 const idx_u64 = Z7IndexUInt64(Z7_INT_ID)
 
@@ -39,7 +38,7 @@ SUITE["Z7IndexUInt64 functions"]["get_digit"] = @benchmarkable get_digit($idx_u6
 SUITE["Z7IndexUInt64 functions"]["get_resolution"] = @benchmarkable get_resolution($idx_u64)
 SUITE["Z7IndexUInt64 functions"]["get_parent"] = @benchmarkable get_parent($idx_u64)
 
-## --- Z7IndexComp functions ---
+# --- Z7IndexComp functions ---
 
 const idx_comp = Z7IndexComp(Z7_INT_ID)
 
@@ -50,23 +49,21 @@ SUITE["Z7IndexComp functions"]["get_digit"] = @benchmarkable get_digit($idx_comp
 SUITE["Z7IndexComp functions"]["get_resolution"] = @benchmarkable get_resolution($idx_comp)
 SUITE["Z7IndexComp functions"]["get_parent"] = @benchmarkable get_parent($idx_comp)
 
-### --- Base Cell Neighbours, GBT neighbours
+# --- neighbours ---
 
 SUITE["neighbours"]["Z7IndexComp get_neighbour"] = @benchmarkable get_neighbour($idx_comp, UInt8(1), 5)
 SUITE["neighbours"]["Z7IndexComp get_neighbours"] = @benchmarkable get_neighbours($idx_comp)
 SUITE["neighbours"]["Z7IndexUInt64 get_neighbour"] = @benchmarkable get_neighbour($idx_u64, UInt8(1), 5)
 SUITE["neighbours"]["Z7IndexUInt64 get_neighbours"] = @benchmarkable get_neighbours($idx_u64)
 
-# Run a quick sanity check if this is executed directly
+# Run when executed directly: julia --project=bench bench/benchmarks.jl
 if abspath(PROGRAM_FILE) == @__FILE__
-    println("Running a sample of benchmarks...")
-    
-    # Run a few benchmarks
-    for group in keys(SUITE)
-        println("\n--- Group: $group ---")
-        # Just run the first one in each group for a quick test
-        name = first(keys(SUITE[group]))
-        b = run(SUITE[group][name], samples=1)
-        println("$name: $(BenchmarkTools.prettytime(minimum(b.times)))")
+    println("Running benchmarks...")
+    results = run(SUITE, verbose=true)
+    for (group, suite) in results
+        println("\n--- $group ---")
+        for (name, trial) in suite
+            println("  $name: $(BenchmarkTools.prettytime(minimum(trial).time))")
+        end
     end
 end
